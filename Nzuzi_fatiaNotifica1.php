ee7c86a51668d720b053d8306dfb1c90c6fd185e
nzuziRodolfo <160322634+nzuziRodolfo@users.noreply.github.com>
2024-03-02 01:48:28 +0100

    Add files via upload

---
+++ Nzuzi_fatiaNotifica1.php
@@ -0,0 +1,14 @@
+<ul class="navbar-nav d-flex justify-content-end mr-2">
+          <!-- Notificatoins -->
+          <li class="nav-item dropdown d-flex align-items-center mr-2">
+            <a class="nav-link nav-link-notifications" id="dropdownNotifications" data-toggle="dropdown" href="#">
+              <i class="oi oi-bell display-inline-block align-middle"></i>
+              <span class="nav-link-notification-number">3</span>
+            </a>
+            <div class="dropdown-menu dropdown-menu-right dropdown-menu-notifications" aria-labelledby="dropdownNotifications">
+              <div class="notifications-header d-flex justify-content-between align-items-center">
+                <span class="notifications-header-title">
+                  Notifications
+                </span>
+                <a href="#" class="d-flex"><small>Mark all as read</small></a>
+              </div>
---
+++ Nzuzi_fatiaPrimeiraDiv.php
@@ -0,0 +1,16 @@
+<div class="adminx-container">
+      <nav class="navbar navbar-expand justify-content-between fixed-top">
+        <a class="navbar-brand mb-0 h1 d-none d-md-block" href="index.html">
+          <img src="./demo/img/logo.png" class="navbar-brand-image d-inline-block align-top mr-2" alt="">
+          AdminX
+        </a>
+
+        <form class="form-inline form-quicksearch d-none d-md-block mx-auto">
+          <div class="input-group">
+            <div class="input-group-prepend">
+              <div class="input-group-icon">
+                <i data-feather="search"></i>
+              </div>
+            </div>
+            <input type="text" class="form-control" id="search" placeholder="Type to search...">
+          </div>
